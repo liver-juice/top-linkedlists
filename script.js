30d837da25d034e56da7ac6b9840287b19cf2f8d
liver-juice <113276155+liver-juice@users.noreply.github.com>
2024-10-05 14:00:11 -0500

    glint of comprehension

---
+++ script.js
@@ -0,0 +1,25 @@
+// step 1: breathe. step 2: think. what the fuck are we doing here. 
+// we have to create the functionaliy to make a linked list.
+// we have to make the functionality to make new nodes in that linked list.
+// we need the methods/algorithms to actually search through that linked list and access our data nodes.
+// this is a linear data structure. linked lists, doubly linked, circular linked.\
+// linked lists do not require a contiguous block in memory. each node is just sprinkled randomly within our node sticks-
+// because they can access eachother through pointers. 
+// linked lists are an entry into more sophisticated data structures and traversals.
+// a data structure is just an efficient way of storing data for a specific task.
+// an object, array, variable, are all data structures.
+// these structures sometimes hold millions of pieces of data, so efficient algorithms are necessary.
+// we calculate an algorithms efficiency in time complexity, typically with Big O notation(most possible steps).
+// something like a binary tree uses the divide and conquer technique, nodes can point to multiple nodes. O(log n);
+// i assume a linked list is O(n), considering we must traverse the entire list starting at the head, calling the pointers to find our value.
+
+
+let linkedList = () => {
+
+    return {};
+}
+
+let Node = () => {
+
+    return {};
+}
